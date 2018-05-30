7b3668ca1da6fc7e6a38389abc86f3ddb02a3951
rehlaender <avilamario16@gmail.com>
2018-05-30 15:01:13 -0700

    generate TextObject component and style

---
+++ src/components/TextObject/TextObjectComponent.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+
+import * as Style from './TextObjectStyle';
+
+class TextObjectComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false,
+      xPosition: 0,
+      yPosition: 200,
+      width: 0,
+      firstRender: true
+    }
+
+    this.toggleEditing = this.toggleEditing.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.onChangeTextObjectValue = this.onChangeTextObjectValue.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+
+    this.generateRandomPosition = this.generateRandomPosition.bind(this);
+    this.returnWindowDimension = this.returnWindowDimension.bind(this);
+    // this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.setWidth = this.setWidth.bind(this);
+  }
+
+  toggleEditing () {
+    const { editing } = this.state;
+    this.setState({editing: !editing});
+  }
+
+  handleClick () {
+    console.log('this.state');
+    this.setState({editing: true});
+  }
+
+  onChangeTextObjectValue (e) {
+    this.props.dispatchChangeValue(this.props.textObject, e.target.value);
+    this.setWidth();
+  }
+
+  handleKeyPress (event) {
+    if(event.key === 'Enter'){
+      window.document.getElementById(`text${this.props.id}`).blur();
+      this.state.firstRender ? this.generateRandomPosition() : null;
+      this.setState({editing: false, firstRender: false});
+    }
+  }
+
+  returnWindowDimension() {
+    const windowSize = {
+      height: window.innerHeight,
+      width: window.innerWidth,
+    }
+    return windowSize;
+  }
+
+  generateRandomPosition() {
+    const windowSize = this.returnWindowDimension();
+    const randomXPosition = (Math.ceil((Math.random() - 0.2) * windowSize.width));
+    const randomYPosition = (Math.ceil((Math.random() - 0.2) * windowSize.height));
+    this.setState({xPosition: randomXPosition, yPosition: randomYPosition});
+    console.log(windowSize, randomXPosition, randomYPosition)
+  }
+
+  setWidth() {
+    const elemntWidth = window.document.getElementById(`text${this.props.id}`).style.width;
+    console.log(elemntWidth, 'this width');
+  }
+
+
+
+  componentDidMount() {}
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  render() {
+    return (
+      <div style={{...Style.ContainerStyle, left: this.state.xPosition, top: this.state.yPosition}}>
+        <span>{this.state.xPosition}, {this.state.yPosition}</span>
+        <input
+          size={this.props.text.length}
+          id={`text${this.props.id}`}
+          readOnly={this.state.editing}
+          style={{...Style.IntputStyle, width: `${this.props.text.length - 1}em`}}
+          value={this.props.text}
+          onClick={() => {this.handleClick}}
+          onBlur={() => {console.log('bye')}}
+          onChange={this.onChangeTextObjectValue}
+          onKeyPress={this.handleKeyPress} />
+     </div>
+    );
+ }
+}
+
+export default TextObjectComponent;
---
+++ src/components/TextObject/TextObjectStyle.js
@@ -0,0 +1,19 @@
+import FontSize from '../../constants/FontSize'
+
+export const ContainerStyle = {
+  position: "absolute",
+  border: "1px solid red",
+  minWidth: "2em",
+}
+
+export const IntputStyle = {
+  background: "none",
+  backgroundColor: "transparent",
+  boxShadow: "none",
+  border: "none",
+  outline: "none",
+  textAlign: "center",
+  height: "1em",
+  minWidth: "2em",
+  fontSize: FontSize.bigFont,
+}
